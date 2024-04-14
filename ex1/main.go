e3f75577aa99773d0b7880f355548c0ebc6206cf
Pawan <pawansharma93520@gmail.com>
2024-04-14 23:17:31 +0530

    ex 2 complete

---
+++ ex1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var packageLevel int = 1
+
+const outSide int = 2
+
+func main() {
+	var insideFunctionint = 3
+	short := 4
+	fmt.Println(packageLevel)
+	fmt.Println(outSide)
+	fmt.Println(insideFunctionint)
+	fmt.Println(short)
+
+}
